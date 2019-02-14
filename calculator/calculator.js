7449f7c6bc9473f301a5270c210712e0d77c07e5
yuena8 <35675503+yuena8@users.noreply.github.com>
2019-02-13 20:10:22 -0700

    formatting

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -3,33 +3,31 @@ var pastEquation = document.getElementById("equation");
 
 
 $(document).ready(function() {
-            let answer = null;
-            let numbers = "1234567890";
-          $('button').on('click', function(e){
-          			$("h1").append("hhh");
-                //let btn_press = e.target.textContent;
-								processButton(e.target.textContent);
-                
-            });
-            //resultElement.value = btn_press;
+  $('button').on('click', function(e){
+    $("h1").append("hhh");
+      //let btn_press = e.target.textContent;
+			processButton(e.target.textContent);
+
+    });
+
 });
 
 function processButton(input){
-			//inputEquation.value += input;
-      if (input==="="){
-					doEvaluation(inputEquation.value);
-      }
-      else if (input==="C"){
-      		inputEquation.value = " ";
-      }
-      else {
-      inputEquation.value += input;
-      }
+  if (input==="="){
+		doEvaluation(inputEquation.value);
+  }
+  else if (input==="C"){
+    inputEquation.value = "";
+    pastEquation.textContent = "";
+  }
+  else {
+    inputEquation.value += input;
+  }
 
 }
 
 function doEvaluation(toCalc){
-      pastEquation.textContent = toCalc;
-			inputEquation.value = eval(toCalc.toString());
+  pastEquation.textContent = toCalc;
+	inputEquation.value = eval(toCalc.toString());
 
 }
